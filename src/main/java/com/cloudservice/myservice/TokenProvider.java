d4a1f8869a2f5f93fc973ed544beb158ec86e5c6
kjy1994 <ghgh415263@naver.com>
2024-10-31 21:21:16 +0900

    jwt 토큰에서 memberId 꺼내는 기능과 만료된 것인지 아닌지 검사하는 기능 추가

--- src/main/java/com/cloudservice/myservice/TokenProvider.java
+++ src/main/java/com/cloudservice/myservice/TokenProvider.java
@@ -1,26 +1,57 @@
 package com.cloudservice.myservice;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.util.Base64;
 import java.util.Date;
 
+@Component
 public class TokenProvider {
 
     private String secretKey = "tmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmp";  //일정 길이 이상으로 해야함
 
     private int ACCESS_TOKEN_EXPIRED_TIME = 1000 * 60 * 30;
 
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
     public Token generateToken(Long id){
         Date now = new Date();
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         String jwtToken = Jwts.builder()
                 .claim("id", id)
                 .expiration(new Date(now.getTime() + ACCESS_TOKEN_EXPIRED_TIME))
-                .signWith(Keys.hmacShaKeyFor(keyBytes))
+                .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey)))
                 .compact();
         return new Token(jwtToken);
+    }
+
+    public Long getMemberId(String jwtToken){
+        Claims claims = getClaims(jwtToken);
+        return claims.get("id", Long.class);
+    }
+
+    public boolean isValidToken(String jwtToken) {
+        Claims claims = getClaims(jwtToken);
+        return claims.getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String jwtToken) {
+        SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+        Claims claims = Jwts.parser()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
+        return claims;
     }
 
 }
--- src/test/java/com/cloudservice/myservice/TokenProviderTest.java
+++ src/test/java/com/cloudservice/myservice/TokenProviderTest.java
@@ -16,7 +16,14 @@ public class TokenProviderTest {
 
     @Test
     void 토큰_생성(){
-        Token token = tokenProvider.generateToken(1l);
+        Token token = tokenProvider.generateToken(111l);
         assertThat(token.getAccessToken()).isNotNull();
+    }
+
+    @Test
+    void 토큰에서_ID가져옴(){
+        String jwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MTExLCJleHAiOjE3MzAzNzg5MDB9.GWRekI9b1dCKFgdE3J4qN90OvsQBXxGVhCEODIKEIhc";
+        Long id = tokenProvider.getMemberId(jwtToken);
+        assertThat(id).isEqualTo(111);
     }
 }
