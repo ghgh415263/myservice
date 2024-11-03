3a874a7c70dc4f37d99ee355b3465fcd02bbffe2
kjy1994 <ghgh415263@naver.com>
2024-11-03 13:05:01 +0900

    oauth 로그인시 세션말고 토큰을 쿠키로 전달하게 바꿈

--- src/main/java/com/cloudservice/myservice/ui/member/OauthController.java
+++ src/main/java/com/cloudservice/myservice/ui/member/OauthController.java
@@ -1,30 +1,37 @@
 package com.cloudservice.myservice.ui.member;
 
+import com.cloudservice.myservice.TokenProvider;
 import com.cloudservice.myservice.application.OauthService;
 import io.micrometer.core.annotation.Counted;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
 @Slf4j
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class OauthController {
 
     private final OauthService oauthService;
 
+    private final TokenProvider tokenProvider;
+
     @Counted("oauth.login")
     @GetMapping("/login/oauth2/code/kakao")
-    public String kakaoCallback(@RequestParam String code, HttpServletRequest httpServletRequest){
+    public String kakaoCallback(@RequestParam String code, HttpServletResponse response){
 
         Long loginMemberId = oauthService.login(code);
 
-        HttpSession session = httpServletRequest.getSession();
-        session.setAttribute("memberId", loginMemberId);
+        Cookie cookie = new Cookie("Authorization", tokenProvider.generateToken(loginMemberId).getAccessToken());
+        cookie.setDomain("localhost");
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
 
-        return "login성공" + loginMemberId;
+        return "redirect:/";
     }
 }
